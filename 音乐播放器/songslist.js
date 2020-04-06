97e073c28934c4687bbb381818f4995149e23653
redhill <1042275917@qq.com>
2020-04-06 22:31:01 +0800

    初始化

---
+++ 音乐播放器/songslist.js
@@ -0,0 +1,27 @@
+var songslist = [{
+        id: 277302,
+        name: "爱",
+        artists: "莫文蔚",
+        picUrl: "http://p1.music.126.net/hcY73QYZt36DeGf91euboQ==/18921495602636668.jpg"
+    },
+    {
+        id: 1313107065,
+        name: "RISE",
+        artists: "The Glitch Mob / Mako / The Word Alive",
+        picUrl: "http://p2.music.126.net/XLulJwVbfowNQhATCt809g==/109951163573081067.jpg"
+    },
+    {
+        id: 5201314,
+        name: "Above All",
+        artists: "Rebecca St. James",
+        picUrl: "http://p2.music.126.net/DlKwvXLjTRbWNII8LHQw8Q==/1798801023051626.jpg"
+    },
+    {
+        id: 1403150208,
+        name: "Phoenix",
+        artists: "-Lil Marron-",
+        picUrl: "http://p2.music.126.net/2zKMCYP_vUyWUUxN3M56kA==/109951164484563377.jpg"
+    }
+];
+// http://music.kele8.cn/
+// https://music.163.com/song/media/outer/url?id=id.mp3
